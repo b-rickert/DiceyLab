634dfafa5e3b4c6e7812ea410735e589dd6b4b41
b-rickert <bd.rickert52@gmail.com>
2025-11-08 13:09:26 -0500

    Completed dice roll and test simulation of 10 random rolls

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,26 @@
+import java.util.Random;
+
 public class Dice {
+int numberOfDice = 2;
+int min = 1;
+int max = 6;
+private Random random;
 
 
+    public Dice(int numberOfDice) {
+    random = new Random();
+    this.numberOfDice = numberOfDice; //class variable -> value passed in
+    }
+
+    public int RandomInt(int min, int max) {
+    return random.nextInt(max - min + 1) + min;
+    }
+
+    public int tossAndSum() {
+    int sum = 0;
+        for (int i = 0; i < numberOfDice; i++) {
+        sum += RandomInt(1, 6);
+        }
+        return sum;
+    }
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,5 +1,13 @@
 public class Simulation {
 
+    public static void main(String[] args) {
+        Dice dice = new Dice(2);
 
+        System.out.println("Testing dice rolls: ");
+        for (int i = 0; i < 10; i++) {
+            int result = dice.tossAndSum();
+            System.out.println("Rolled " + (i+1) + ": " + result);
+        }
 
+    }
 }
