f2f0d89e677c3993628ede5f5cbd7f10cbe2a7be
b-rickert <bd.rickert52@gmail.com>
2025-11-08 20:38:08 -0500

    Almost finished, just need to add print stars

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,4 +1,24 @@
 
-public class Bins {
+// Bins array to hold counts for every possible total between min and max rolls
 
+public class Bins {
+    private int[] bins;
+    private int minRoll;
+    private int maxRoll;
+
+    public Bins(int minRoll, int maxRoll) {
+        this.minRoll = minRoll;
+        this.maxRoll = maxRoll;
+        this.bins = new int[maxRoll - minRoll + 1];
+    }
+
+    public void incrementBin(int binNum) {
+        int index = binNum - minRoll;  // tells where in the array bin belongs
+        bins[index]++; // Takes current stored bin and adds 1 and re-stores it back
+    }
+
+    public int getBin(int binNum) {
+        int index = binNum - minRoll;
+        return bins[index];
+    }
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,13 +1,38 @@
 public class Simulation {
+    private Dice dice;
+    private Bins bins;
+    private int numOfTosses;
+
+    public Simulation(int numberOfDice, int numOfTosses) {
+        this.dice = new Dice(numberOfDice);
+        this.bins = new Bins(numberOfDice, numberOfDice * 6);
+        this.numOfTosses = numOfTosses;
+    }
+
+    public void runSimulation() {
+        for (int i = 0; i < numOfTosses; i++) {
+            int result = dice.tossAndSum(); // rolls the dice
+            bins.incrementBin(result); // records our result
+        }
+    }
+
+    public void printResults() {
+        System.out.println("***");
+        System.out.println("Simulation of 2 dice tossed 1000000 times");
+        System.out.println("***");
+
+        for (int i = 2; i <= 12; i++) {
+            int count = bins.getBin(i); 
+            double percentage = (double) count / numOfTosses;
+            System.out.printf("%2d : %8d: %.2f\n" , i, count, percentage);
+            
+
+        }
+    }
 
     public static void main(String[] args) {
-        Dice dice = new Dice(2);
-
-        System.out.println("Testing dice rolls: ");
-        for (int i = 0; i < 10; i++) {
-            int result = dice.tossAndSum();
-            System.out.println("Rolled " + (i+1) + ": " + result);
-        }
-
+        Simulation sim = new Simulation(2, 1000000);
+        sim.runSimulation();
+        sim.printResults();
     }
 }
